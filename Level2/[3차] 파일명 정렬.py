38480ceb4ab4edb4476d44837ecace5387b602fa
KimGeunUk <kgw5430@naver.com>
2022-09-29 22:31:47 +0900

    Level2

--- Level2/[3차] 파일명 정렬.py
+++ Level2/[3차] 파일명 정렬.py
@@ -46,6 +46,9 @@ def solution(files):
     for i in files:
         print(i.upper())
     
+    ##
+    
+    
     return answer
 
 print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
