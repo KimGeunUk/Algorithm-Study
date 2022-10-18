98227727302b5f03b540a2d8df88af5692588f41
KimGeunUk <kgw5430@naver.com>
2022-10-18 15:18:20 +0900

    Level2

--- Level2/후보키.py
+++ Level2/후보키.py
@@ -8,8 +8,9 @@
 후보키에 대한 내용이 잘 기억나지 않던 제이지는, 정확한 내용을 파악하기 위해 데이터베이스 관련 서적을 확인하여 아래와 같은 내용을 확인하였다.
 
 관계 데이터베이스에서 릴레이션(Relation)의 튜플(Tuple)을 유일하게 식별할 수 있는 속성(Attribute) 또는 속성의 집합 중, 다음 두 성질을 만족하는 것을 후보 키(Candidate Key)라고 한다.
-유일성(uniqueness) : 릴레이션에 있는 모든 튜플에 대해 유일하게 식별되어야 한다.
-최소성(minimality) : 유일성을 가진 키를 구성하는 속성(Attribute) 중 하나라도 제외하는 경우 유일성이 깨지는 것을 의미한다. 즉, 릴레이션의 모든 튜플을 유일하게 식별하는 데 꼭 필요한 속성들로만 구성되어야 한다.
+    유일성(uniqueness) : 릴레이션에 있는 모든 튜플에 대해 유일하게 식별되어야 한다.
+    최소성(minimality) : 유일성을 가진 키를 구성하는 속성(Attribute) 중 하나라도 제외하는 경우 유일성이 깨지는 것을 의미한다. 즉, 릴레이션의 모든 튜플을 유일하게 식별하는 데 꼭 필요한 속성들로만 구성되어야 한다.
+    
 제이지를 위해, 아래와 같은 학생들의 인적사항이 주어졌을 때, 후보 키의 최대 개수를 구하라.
 
 cand_key1.png
@@ -21,3 +22,81 @@ cand_key1.png
 
 릴레이션을 나타내는 문자열 배열 relation이 매개변수로 주어질 때, 이 릴레이션에서 후보 키의 개수를 return 하도록 solution 함수를 완성하라.
 """
+from itertools import combinations as C
+def solution(relation):
+    answer = []
+    
+    rows = len(relation)
+    columns = [i for i in range(len(relation[0]))]
+    
+    for n in range(1, len(relation[0])+1):
+        check = list(C(columns, n))
+        for c in check:
+            row_l = []            
+            isCandi = True
+            
+            for row in range(rows):
+                chain=''
+                for c_ in c:
+                    chain += str(relation[row][c_])
+                    
+                if chain in row_l:
+                    isCandi = False
+                    break
+                else:
+                    isCandi = True
+                    row_l.append(chain)
+                    
+            if isCandi == True:
+                if len(c) == 1:
+                    answer.append(c)
+                    for c_ in c:
+                        columns.pop(columns.index(c_))
+                else:
+                    isin = False
+                    for an in answer:
+                        count = 0                        
+                        for a in an:
+                            if a in c:
+                                count += 1
+                        if count == len(an):
+                           isin = True
+                           break
+                        else:
+                            isin = False
+                    if isin == False:
+                        answer.append(c)
+    
+    return answer
+
+# 다른 사람 풀이
+from itertools import combinations
+def solution1(relation):
+    n_row=len(relation)
+    n_col=len(relation[0])  #->runtime error 우려되는 부분
+
+    candidates=[]
+    for i in range(1,n_col+1):
+        candidates.extend(combinations(range(n_col),i))
+
+    final=[]
+    for keys in candidates:
+        tmp=[tuple([item[key] for key in keys]) for item in relation]
+        if len(set(tmp))==n_row:
+            final.append(keys)
+    print(final)
+    answer=set(final[:])
+    for i in range(len(final)):
+        for j in range(i+1,len(final)):
+            if len(final[i])==len(set(final[i]).intersection(set(final[j]))):
+                answer.discard(final[j])
+
+    return len(answer)
+
+print(solution1([["100","ryan","music","2"],
+                ["200","apeach","math","2"],
+                ["300","tube","computer","3"],
+                ["400","con","computer","4"],
+                ["500","muzi","music","3"],
+                ["600","apeach","music","2"]])) # 2
+# print(solution([["a","1","aaa","c","ng"], ["a","1","bbb","e","g"], ["c","1","aaa","d","ng"], ["d","2","bbb","d","ng"]]))
