6cda7d001386295ec042efda2cbaf1a06fda645b
KimGeunUk <kgw5430@naver.com>
2022-11-09 17:52:46 +0900

    기본 수학 1

--- STEP/기본 수학 1/설탕 배달.py
+++ STEP/기본 수학 1/설탕 배달.py
@@ -1,14 +1,36 @@
 N = int(input())
 
-if (N - 3) % 5 == 0:
-    answer = (N // 5) + (N % 5) // 3
-elif (N - 5) % 3 == 0:
-    answer = (N // 3) + (N % 3) // 5
-elif N % 5 == 0:
-    answer = N // 5
-elif N % 3 == 0:
-    answer = N // 3
-else:
-    answer = -1
+i = 0
+answer = []
+while True:
+    if (N - (5 * i)) % 3 == 0:
+        print(i)
+        answer.append(i + ((N - (5 * i)) // 3))
+    elif (N - (3 * i)) % 5 == 0:
+        print(i)
+        answer.append(i + ((N - (3 * i)) // 5))
+
+    
+    i += 1    
+    
+    if (N - (5 * i)) < 0 or (N - (3 * i)) < 0:
+        break
     
 print(answer)
+# 시간 초과
+# answer = []
+# 
+# def dfs(n, s):    
+#     if n < 0: return
+#     elif n == 0:
+#         answer.append(s)
+#         return
+
+#     dfs(n-5, s+1)
+#     dfs(n-3, s+1)
+    
+# s = 0
+# dfs(N, s)
+
+# if len(answer) == 0: print(-1)
+# else: print(min(answer))
