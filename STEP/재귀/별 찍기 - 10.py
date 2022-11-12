7075e2210ea7b0dc73b2833abda8958a253a9a54
KimGeunUk <kgw5430@naver.com>
2022-11-12 23:29:03 +0900

    재귀

---
+++ STEP/재귀/별 찍기 - 10.py
@@ -0,0 +1,23 @@
+
+def star_(n):
+    if n == 1:
+        return ['*']
+    
+    stars = star_(n//3)
+    
+    star = []
+    
+    for s in stars:
+        star.append(s*3)
+    
+    for s in stars:
+        star.append(s + ' '*(n//3) + s)
+   
+    for s in stars:
+        star.append(s*3)
+
+    return star
+
+N = int(input())
+
+print('\n'.join(star_(N)))
