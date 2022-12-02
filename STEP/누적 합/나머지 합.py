4f9baa41e3e6322f52fed0273922ae607ea644df
KimGeunUk <kgw5430@naver.com>
2022-12-02 17:45:39 +0900

    누적 합

--- STEP/누적 합/나머지 합.py
+++ STEP/누적 합/나머지 합.py
@@ -4,12 +4,16 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 L = list(map(int, input().split()))
+array = [0 for _ in range(N+1)] # 누적 합
 
-array = [0 for _ in range(N+1)]
-
-c = 0
+res = [0] * M
 
 for i in range(1, N+1):
-    array[i] = (L[i-1] + array[i-1]) % 3
+    array[i] = L[i-1] + array[i-1]
+    res[array[i-1] % M] += 1
 
-print(array)
+c = res[0]
+for r in res:
+    c += r * (r-1) // 2
+
+print(c)
