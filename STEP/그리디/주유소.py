9cafa8bf037ec00a461a0e850111fbb3cd5a1d90
KimGeunUk <kgw5430@naver.com>
2022-12-04 23:57:11 +0900

    그리디

--- STEP/그리디/주유소.py
+++ STEP/그리디/주유소.py
@@ -5,14 +5,10 @@ r = list(map(int, input().split())) + [0]
 
 answer = 0
 idx = 0
+
 for i in range(1, N+1):    
     if r[idx] >= r[i]:
-        print(i)
         answer += r[idx] * sum(l[idx:i])
         idx = i
 
 print(answer)
-
-50
-150
-80
