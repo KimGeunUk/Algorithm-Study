fcc535452227e5881b20f4d9cd7eb39ff6306dcb
KimGeunUk <kgw5430@naver.com>
2022-12-05 00:00:03 +0900

    그리디

--- STEP/그리디/회의실 배정.py
+++ STEP/그리디/회의실 배정.py
@@ -1,11 +1,15 @@
 import sys
-N = int(input())
-M = []
-end, mcount = 0, 0
-for i in range(N):
-    M.append(list(map(int,sys.stdin.readline().split())))
-M = sorted(M,key = lambda x: (x[1],x[0]))
+input = sys.stdin.readline
 
+N = int(input())
+
+M = []
+for i in range(N):
+    M.append(list(map(int, input.split())))
+
+M = sorted(M,key = lambda x: (x[1], x[0]))
+
+end, mcount = 0, 0
 for i in range(N):
     if end <= M[i][0]:
         mcount += 1
