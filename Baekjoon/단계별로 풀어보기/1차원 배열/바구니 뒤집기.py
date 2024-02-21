53cdf542b6d21558a09fdf3a03f7020472ec3679
KimGeunUk <kgw5430@naver.com>
2024-02-21 18:50:02 +0900

    Add: 20240221

---
+++ Baekjoon/단계별로 풀어보기/1차원 배열/바구니 뒤집기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+basket = [i for i in range(n+1)]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    
+    basket[i:j+1] = basket[j:i-1:-1]
+    
+print(" ".join(map(str, basket[1:])))
---
+++ Baekjoon/단계별로 풀어보기/입출력과 사칙연산/꼬마 정민.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+
+_str = map(int, input().split(' '))
+
+print(sum(_str))
