30c73346a908bfd7e8257c234127437e0c2150a8
KimGeunUk <kgw5430@naver.com>
2024-02-26 23:39:17 +0900

    Add: 20240226

---
+++ Baekjoon/단계별로 풀어보기/약수, 배수와 소수/약수들의 합.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    n = int(input())
+    
+    if n == -1: break
+    
+    _sum = [1]
+    for i in range(2, int(n**(1/2))+1):
+        if n % i == 0:            
+            _sum.extend([i, int(n//i)])
+    
+    if sum(_sum) == n:
+        _sum = sorted(_sum)
+        print(f"{n} = ", end='')
+        print(" + ".join(map(str, _sum)))
+    else:
+        print(f"{n} is NOT perfect.")
+        
+    
